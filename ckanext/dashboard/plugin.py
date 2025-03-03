ba3832ffe3f2a962fff24b59204db8e32e51ae1d
german <germankay@gmail.com>
2025-03-03 15:54:45 -0300

    Traducciones Andando

--- ckanext/dashboard/plugin.py
+++ ckanext/dashboard/plugin.py
@@ -8,15 +8,17 @@ from ckanext.dashboard.actions.dashboard_dataset import (dataset_dashboard_list,
                                                          dataset_dashboard_delete)
 from ckanext.dashboard.auth import dashboard_dataset as auth
 from ckanext.dashboard.helpers import get_dataset_dashboard
+from ckan.lib.plugins import DefaultTranslation
 
 
-class DashboardPlugin(p.SingletonPlugin):
+class DashboardPlugin(p.SingletonPlugin, DefaultTranslation):
     """Plugin for managing dashboards in CKAN"""
     p.implements(p.IConfigurer)
     p.implements(p.IBlueprint)
     p.implements(p.IActions)
     p.implements(p.ITemplateHelpers)
     p.implements(p.IAuthFunctions)
+    p.implements(p.ITranslation)
 
     # IConfigurer
 
@@ -56,3 +58,13 @@ class DashboardPlugin(p.SingletonPlugin):
 
     def get_helpers(self):
         return {'get_dataset_dashboard': get_dataset_dashboard}
+
+    def i18n_locales(self):
+        """Lanaguages this plugin has translations for."""
+        # Return a list of languages that this plugin has translations for.
+        return ["es", "en"]
+
+    def i18n_domain(self):
+        """The domain for this plugin's translations."""
+        # Return the translation domain for this plugin.
+        return "ckanext-dashboard"
