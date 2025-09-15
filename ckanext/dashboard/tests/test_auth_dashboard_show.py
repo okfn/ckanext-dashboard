56d6bd1dabd60bc275b8adf1f5707005f293a098
German Favaro <germankay@gmail.com>
2025-09-15 12:48:49 -0300

    fix test

--- ckanext/dashboard/tests/test_auth_dashboard_show.py
+++ ckanext/dashboard/tests/test_auth_dashboard_show.py
@@ -3,38 +3,41 @@ from ckan.plugins import toolkit
 from ckan.tests import factories, helpers
 
 
-
 @pytest.mark.usefixtures('with_plugins', 'clean_db')
 class TestDashboardDatasetShowAuth:
     def test_public_dataset_is_viewable_by_anonymous(self):
+        """Anyone can view the dashboard of a public dataset."""
         org = factories.Organization()
         ds = factories.Dataset(owner_org=org["id"], private=False)
 
-        # anonymous
-        context = {"user": ""}
+        context = {"user": ""}  # anonymous
         assert toolkit.check_access("dataset_dashboard_show", context, {"id": ds["id"]}) is True
 
     def test_private_dataset_denied_to_anonymous(self):
+        """Anonymous users cannot view the dashboard of a private dataset."""
         org = factories.Organization()
         ds = factories.Dataset(owner_org=org["id"], private=True)
 
-        context = {"user": ""}
-        assert toolkit.check_access("dataset_dashboard_show", context, {"id": ds["id"]}) is False
+        context = {"user": ""}  # anonymous
+        with pytest.raises(toolkit.NotAuthorized):
+            toolkit.check_access("dataset_dashboard_show", context, {"id": ds["id"]})
 
     def test_private_dataset_denied_to_non_member_user(self):
+        """A non-member user cannot view the dashboard of a private dataset."""
         org = factories.Organization()
         ds = factories.Dataset(owner_org=org["id"], private=True)
         user = factories.User()
 
         context = {"user": user["name"]}
-        assert toolkit.check_access("dataset_dashboard_show", context, {"id": ds["id"]}) is False
+        with pytest.raises(toolkit.NotAuthorized):
+            toolkit.check_access("dataset_dashboard_show", context, {"id": ds["id"]})
 
     def test_private_dataset_allowed_to_org_member(self):
+        """A member user can view the dashboard of a private dataset."""
         org = factories.Organization()
         ds = factories.Dataset(owner_org=org["id"], private=True)
         user = factories.User()
 
-        # dar rol lector (member) en la org
         helpers.call_action(
             "organization_member_create",
             id=org["id"],
@@ -46,6 +49,7 @@ class TestDashboardDatasetShowAuth:
         assert toolkit.check_access("dataset_dashboard_show", context, {"id": ds["id"]}) is True
 
     def test_private_dataset_allowed_to_sysadmin(self):
+        """A sysadmin can view the dashboard of a private dataset."""
         org = factories.Organization()
         ds = factories.Dataset(owner_org=org["id"], private=True)
         admin = factories.Sysadmin()
